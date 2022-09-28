60b52addb099dd8d9414227e00ed9181c5bdf329
Pedro Pimentinha <pedro.pimentinha18@gmail.com>
2022-09-28 21:03:09 +0200

    first commit

---
+++ public/js/app.js
@@ -0,0 +1,22 @@
+const counter = document.querySelector('.counter');
+const startButton = document.querySelector('.btnStart');
+
+let initialTime = 45;
+
+startButton.addEventListener('click', timerInt);
+
+function timerInt() {
+  if (initialTime === 0) {
+    initialTime = 45;
+  } else {
+    setInterval(timer, 1000);
+  }
+}
+
+function timer() {
+  if (initialTime === 0) return;
+
+  initialTime--;
+  // prettier-ignore
+  return counter.innerHTML = initialTime;
+}
---
+++ server.js
@@ -0,0 +1,32 @@
+import express from 'express';
+const app = express();
+import helmet from 'helmet';
+import compression from 'compression';
+
+/* //error handling
+import 'express-async-errors'; */
+
+app.use(express.static('public'));
+/* app.use(express.static(path.join(__dirname, '/public/css'))); */
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + './public/index.html');
+});
+
+app.use(express.json());
+app.use(helmet());
+app.use(compression());
+
+const port = process.env.PORT || 5000;
+
+const start = () => {
+  try {
+    app.listen(port, () => {
+      console.log(`Server is listening in port ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
