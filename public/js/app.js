87d122a98768f3115327e1f71701ef05faddeaf2
Pedro Pimentinha <pedro.pimentinha18@gmail.com>
2022-10-24 18:27:48 +0200

    the page will not restart when the countdown reaches 0. Instead will return to the initial state defined by the user. added var userTime that will store the value defined by the user

--- public/js/app.js
+++ public/js/app.js
@@ -7,16 +7,26 @@ startButton.addEventListener('click', timerInt);
 upButton.addEventListener('click', add);
 downButton.addEventListener('click', sub);
 
-let initialTime = 45;
+let userTime = 5;
+let initialTime = 5;
 counter.innerHTML = initialTime;
+let interval;
 
 function timerInt() {
   startButton.setAttribute('disabled', '');
-  setInterval(timer, 1000);
+  interval = setInterval(timer, 1000);
 }
 
 function timer() {
-  if (initialTime === 0) return location.reload();
+  if (initialTime === 0) {
+    // startButton.removeEventListener('click', timerInt);
+    startButton.removeAttribute('disabled', '');
+    clearInterval(interval);
+    initialTime = userTime;
+    counter.innerHTML = initialTime;
+    return;
+  }
+
   initialTime--;
   // prettier-ignore
   return counter.innerHTML = initialTime;
@@ -24,11 +34,13 @@ function timer() {
 
 function add() {
   initialTime += 1;
+  userTime += 1;
   counter.innerHTML = initialTime;
 }
 
 function sub() {
   if (initialTime === 0) return;
   initialTime -= 1;
+  userTime -= 1;
   counter.innerHTML = initialTime;
 }
